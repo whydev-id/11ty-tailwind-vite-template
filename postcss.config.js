cb1caa6ef9bc5e13894dc9de2243b3933638f39f
whydev-id <hello.wyudi@gmail.com>
2024-11-16 20:05:56 +0700

    feat: add postcss & tailwind configs

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: {
+		"postcss-import": {},
+		"postcss-nesting": {},
+		"postcss-combine-media-query": {},
+		tailwindcss: {},
+		autoprefixer: {},
+		cssnano: {
+			preset: "default",
+		},
+	},
+};
