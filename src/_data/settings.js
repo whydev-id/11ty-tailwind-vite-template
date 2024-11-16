191b2dc3d45ea63df2fec86a4ac5c0ee86613a2f
whydev-id <hello.wyudi@gmail.com>
2024-11-16 19:57:24 +0700

    feat: add assets files

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -5,7 +5,6 @@ const settings = {
 	description: "Personal website with 11ty engine",
 	url: "http://localhost:8080", // change with your domain
 	theme_color: "#2f4858",
-	icon_sizes: [144, 192, 512],
 	currentYear,
 };
 
